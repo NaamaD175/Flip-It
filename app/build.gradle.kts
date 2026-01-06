87b8246f1746577e8db17fa1c4db61a3dea6d7b0
NaamaD175 <naama.drory@gmail.com>
2026-01-06 10:17:44 +0200

    Application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 }
